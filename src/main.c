8947a55f1c5a5f6a36a351ab705d3ee9687c634e
Charles <charles.rosenbauer@gmail.com>
2023-05-20 23:50:52 -0500

    File loading, some notes on serialization and operations

--- src/main.c
+++ src/main.c
@@ -7,6 +7,13 @@
 #include "util.h"
 
 
-int main(){
-	printf("hello gromov\n");
+int main(int ac, char** av){
+	uint8_t** bytes = alloca(sizeof(uint8_t ) * ac);
+	int64_t*  sizes = alloca(sizeof(uint64_t) * ac);
+	for(int i = 1; i < ac; i++){
+		printf("Loading %s...\n", av[i]);
+		if(!loadFile(av[i], (char**)&bytes[i], &sizes[i])){
+			printf("  Could not load %s\n", av[i]);
+		}
+	}
 }
---
+++ src/serde.c
@@ -0,0 +1,18 @@
+#include "stdint.h"
+#include "stdlib.h"
+#include "stdio.h"
+
+#include "struct.h"
+#include "util.h"
+
+
+
+/*
+	We need to parse a byte stream based on a specification and transform it
+	into a data structure. For debugging, etc. we probably want to also include
+	the inverse as well.
+	
+	Additionally, we need operations on it: construct trees, traverse trees,
+	read contents, edit contents, check types, check identifiers, etc.
+*/
+
